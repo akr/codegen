f47a3ab3f22fd0648cb89be58312851012316961
Tanaka Akira <akr@fsij.org>
2020-06-24 00:01:03 +0900

    update sample/ind.v for new code generator.

--- sample/ind.v
+++ sample/ind.v
@@ -1,21 +1,27 @@
 From codegen Require Import codegen.
 
-CodeGen Inductive Type bool "bool".
-CodeGen Inductive Constructor bool true "true".
-CodeGen Inductive Constructor bool false "false".
-CodeGen Inductive Match bool "" true "default" false "case 0".
+CodeGen Inductive Type bool => "bool".
+CodeGen Inductive Match bool => ""
+| true => "default"
+| false => "case 0".
+CodeGen Constant true => "true".
+CodeGen Constant false => "false".
 Print CodeGen Inductive bool.
 
-CodeGen Inductive Type nat "uint64_t".
-CodeGen Inductive Constructor nat O "0".
-CodeGen Inductive Constructor nat S "succ".
-CodeGen Inductive Match nat "" O "case 0" S "default" "pred".
+CodeGen Inductive Type nat => "nat".
+CodeGen Inductive Match nat => ""
+| O => "case 0"
+| S => "default" "predn".
+CodeGen Constant O => "0".
+CodeGen Primitive S => "succn".
 Print CodeGen Inductive nat.
 
-CodeGen Inductive Type (option bool) "int".
-CodeGen Inductive Constructor (option bool) None "(-1)".
-CodeGen Inductive Constructor (option bool) Some "(int)".
-CodeGen Inductive Match (option bool) "" None "case -1" Some "default" "".
+CodeGen Inductive Type (option bool) => "int".
+CodeGen Inductive Match (option bool) => ""
+| None => "case -1"
+| Some => "default" "".
+CodeGen Constant None bool => "(-1)".
+CodeGen Primitive Some bool => "(int)".
 Print CodeGen Inductive (option bool).
 
 Definition opt_bool := option bool.
@@ -23,5 +29,5 @@ Print CodeGen Inductive (option bool).
 
 Print CodeGen Inductive.
 
-CodeGen Monomorphization Nat.add.
-CodeGen GenC _add.
+CodeGen Function Nat.add.
+CodeGen Gen "add".
