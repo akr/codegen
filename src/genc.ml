e622b53d0d027e2f23dd6c21ccce4233d160dac7
Tanaka Akira <akr@fsij.org>
2021-08-10 13:48:32 +0900

    move IntSet definition to cgenutil.ml.

--- src/genc.ml
+++ src/genc.ml
@@ -16,9 +16,6 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 *)
 
-module IntSet = Set.Make(Int)
-
-
 open Names
 open Pp
 open CErrors
