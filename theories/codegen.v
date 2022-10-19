1c74a423fac5bc5957530f5ab0f9a6b6c7e5f12e
Tanaka Akira <akr@fsij.org>
2022-10-19 14:11:42 +0900

    don't require FunctionalExtensionality.

--- theories/codegen.v
+++ theories/codegen.v
@@ -16,7 +16,4 @@ License along with this library; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 *)
 
-Require Coq.Logic.FunctionalExtensionality.
-Register Coq.Logic.FunctionalExtensionality.functional_extensionality as codegen.functional_extensionality.
-
 Declare ML Module "coq-codegen.plugin".
