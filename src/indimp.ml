4a3055aa35edf39295c5fb27a71368cb615633b8
Tanaka Akira <akr@fsij.org>
2024-02-27 22:52:18 +0900

    generate_indimp_names refined.

--- src/indimp.ml
+++ src/indimp.ml
@@ -119,8 +119,8 @@ let generate_indimp_names (env : Environ.env) (sigma : Evd.evar_map) (coq_type :
   let global_prefix = global_gensym () in
   let (f, args) = decompose_appvect sigma coq_type in
   let params = array_rev args in (* xxx: args should be parameters of inductive type *)
-  let (ind, u) = destInd sigma f in
-  let (mutind, _) = ind in
+  let pind = destInd sigma f in
+  let ((mutind, i), u) = pind in
   let mutind_body = Environ.lookup_mind mutind env in
   check_ind_id_conflict mutind_body;
   let open Declarations in
