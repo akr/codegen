f559fedf50fc287811612374500a2a023e45e4fe
Tanaka Akira <akr@fsij.org>
2024-06-24 14:55:01 +0900

    set current header/source filename via function.

--- src/filegen.ml
+++ src/filegen.ml
@@ -28,6 +28,12 @@ let defined_sections = [
   "epilogue";
 ]
 
+let codegen_set_headerfile (filename : string) : unit =
+  current_header_filename := filename
+
+let codegen_set_sourcefile (filename : string) : unit =
+  current_source_filename := filename
+
 let codegen_add_generation (filename : string) (generation : code_generation) : unit =
   generation_map := !generation_map |> CString.Map.update filename
     (fun generation_list_opt ->
--- src/filegen.mli
+++ src/filegen.mli
@@ -1,6 +1,8 @@
 open State
 
 val defined_sections : string list
+val codegen_set_headerfile : string -> unit
+val codegen_set_sourcefile : string -> unit
 val codegen_add_generation : string -> code_generation -> unit
 val codegen_add_source_generation : code_generation -> unit
 val codegen_add_header_generation : code_generation -> unit
