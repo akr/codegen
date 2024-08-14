66b17c7a08c498b694cc273cda67eb66d3d163da
Tanaka Akira <akr@fsij.org>
2024-08-14 14:43:32 +0900

    make ascii and string as eqType.

--- sample/str/ascii.v
+++ sample/str/ascii.v
@@ -1,7 +1,36 @@
+From mathcomp Require Import all_ssreflect.
+From HB Require Import structures.
+
 Require Import Ascii.
 (*
 Inductive ascii : Set :=
   Ascii : (*LSB*)bool -> bool -> bool -> bool ->
           bool -> bool -> bool -> (*MSB*)bool -> ascii.
 *)
+
+
+Open Scope string_scope. (* enable "string-literal" and str ++ str *)
+Open Scope seq_scope. (* prefer seq ++ seq over str ++ str *)
+
+Definition eqascii a b :=
+  let: Ascii a1 a2 a3 a4 a5 a6 a7 a8 := a in
+  let: Ascii b1 b2 b3 b4 b5 b6 b7 b8 := b in
+  (a1 == b1) && (a2 == b2) && (a3 == b3) && (a4 == b4) &&
+  (a5 == b5) && (a6 == b6) && (a7 == b7) && (a8 == b8).
+
+Lemma eqasciiP : Equality.axiom eqascii.
+Proof.
+  move=> a b.
+  apply: (iffP idP).
+    case: a => a1 a2 a3 a4 a5 a6 a7 a8.
+    case: b => b1 b2 b3 b4 b5 b6 b7 b8.
+    rewrite /eqascii.
+    do 7 rewrite andbC => /andP [] /eqP ->.
+    by move=> /eqP ->.
+  move=> ->.
+  case: b => b1 b2 b3 b4 b5 b6 b7 b8 /=.
+  by do 8 rewrite eq_refl.
+Qed.
+
+HB.instance Definition _ := hasDecEq.Build ascii eqasciiP.
 
--- sample/str/str.v
+++ sample/str/str.v
@@ -1,3 +1,6 @@
+From mathcomp Require Import all_ssreflect.
+From HB Require Import structures.
+
 Require Import nat.
 Require Import ascii.
 
@@ -7,3 +10,43 @@ Inductive string : Set :=
 | EmptyString : string
 | String : ascii -> string -> string.
 *)
+
+Fixpoint seq_of_str str :=
+  match str with
+  | "" => nil
+  | String c str' => c :: (seq_of_str str')
+  end.
+
+Fixpoint str_of_seq s :=
+  match s with
+  | nil => ""
+  | c :: s' => String c (str_of_seq s')
+  end.
+
+Lemma str_of_seq_of_str str : str_of_seq (seq_of_str str) = str.
+Proof. by elim: str => [|c str /= ->]. Qed.
+
+Lemma seq_of_str_of_seq s : seq_of_str (str_of_seq s) = s.
+Proof. by elim: s => [|c s /= ->]. Qed.
+
+Coercion seq_of_str : string >-> seq.
+
+Definition eqstr a b := seq_of_str a == seq_of_str b.
+
+Lemma eqstr_eq a b : (eqstr (str_of_seq a) (str_of_seq b)) = (a == b).
+Proof. by rewrite /eqstr 2!seq_of_str_of_seq. Qed.
+
+Lemma eqstrP : Equality.axiom eqstr.
+Proof.
+  move=> a' b'.
+  rewrite -(str_of_seq_of_str a') -(str_of_seq_of_str b').
+  move: (seq_of_str a') (seq_of_str b') => a b.
+  clear a' b'.
+  rewrite eqstr_eq.
+  apply: (iffP idP).
+    by move=> /eqP ->.
+  rewrite -{2}(seq_of_str_of_seq a) => ->.
+  by rewrite seq_of_str_of_seq.
+Qed.
+
+HB.instance Definition _ := hasDecEq.Build string eqstrP.
